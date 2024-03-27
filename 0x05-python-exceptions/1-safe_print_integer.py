89ddfc630a4d966255182392d80ca310f3faea4b
Mitchelle-Muhambi <mitchellemuhambi7@gmail.com>
2024-03-26 17:58:11 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except IndexError:
+    except BaseException:
         return False
+
+
+
+
+
+
+
+
